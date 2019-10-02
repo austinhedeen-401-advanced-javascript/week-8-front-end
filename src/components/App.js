0ef87a22a8ffbf64f13839bce83388839791a2af
Austin Hedeen <austin.hedeen@gmail.com>
2019-10-02 00:30:13 -0700

    Update App to render auth examples

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
+import Auth from './auth/auth';
+import Login from './auth/login';
+import LoginProvider from './auth/context';
+
+function Read() {
+  return (
+    <Auth capability="read">
+      <span>Signed in! You have &quot;read&quot; capabilities.</span>
+    </Auth>
+  );
+}
+
+function Update() {
+  return (
+    <Auth capability="update">
+      <span>You have &quot;update&quot; capabilities.</span>
+    </Auth>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
+    <LoginProvider>
       <h1>Hello world!</h1>
-    </div>
+      <Login />
+      <hr />
+      <Read />
+      <Update />
+    </LoginProvider>
   );
 }
 
