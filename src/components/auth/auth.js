374ba7dbfc2eb21092cbd27c215387fd0a283c47
Austin Hedeen <austin.hedeen@gmail.com>
2019-10-02 00:29:10 -0700

    Implement an Auth component to conditionally render based on user capability

---
+++ src/components/auth/auth.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { LoginContext } from './context';
+
+const If = (props) => {
+  return props.condition ? props.children : null;
+};
+
+class Auth extends React.Component {
+  static contextType = LoginContext;
+
+  render() {
+    let okToRender = false;
+    try {
+      const { loggedIn, user } = this.context;
+      const { capability } = this.props;
+      okToRender = loggedIn && capability && user.capabilities.includes(capability);
+    } catch (e) {
+      console.warn('Not Authorized');
+    }
+
+    return (
+      <If condition={okToRender}>
+        <div>{ this.props.children }</div>
+      </If>
+    );
+  }
+}
+
+Auth.propTypes = {
+  capability: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Auth;
